08ce23bcdbc01cda8385c03b71e76a17aa932ec9
odmrs <omarcosviniciusdev@gmail.com>
2025-06-11 10:34:55 -0300

    feat: create insert tail (draft)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 typedef struct Node {
-  int value;
+  int data;
   struct Node *next;
 } Node;
 
@@ -15,44 +15,65 @@ typedef struct {
 } LinkedList;
 
 Node *createNode(int value) {
-  Node *newNode = malloc(sizeof(Node));
+  Node *newNode = (Node *)malloc(sizeof(Node));
   if (newNode == NULL) {
     return NULL;
   }
-
+  newNode->data = value;
   newNode->next = NULL;
-  newNode->value = value;
 
   return newNode;
 }
 
 void insertHead(LinkedList *ll, int value) {
-  Node *newNode = createNode(value);
+  Node *node = createNode(value);
 
-  newNode->next = ll->head;
-  ll->head = newNode;
+  node->next = ll->head;
+  ll->head = node;
 }
 
 void printLinkedList(LinkedList *ll) {
+  printf("Linkedlist: ");
   Node *cursor = ll->head;
-
-  printf("Linked List: ");
   while (cursor != NULL) {
-    printf("%d -> ", cursor->value);
+    printf("%d -> ", cursor->data);
     cursor = cursor->next;
   }
 
-  printf("NULL;\n");
+  printf("NULL\n");
+}
+
+int linkedListIsEmpty(LinkedList *ll) {
+  if (ll->head == NULL) {
+    return 1;
+  }
+
+  return 0;
+}
+
+void insertNodeTail(LinkedList *ll, int value) {
+  if (linkedListIsEmpty(ll)) {
+    return;
+  }
+  Node *newNode = createNode(value);
+  Node *cursor = ll->head;
+  while (cursor->next != NULL) {
+    cursor = cursor->next;
+  }
+
+  cursor->next = newNode;
+  newNode->next = NULL;
 }
 
 int main() {
   LinkedList *intList = (LinkedList *)malloc(sizeof(LinkedList));
   intList->head = NULL;
-  insertHead(intList, 5);
-  insertHead(intList, 5);
-  insertHead(intList, 5);
-  insertHead(intList, 5);
-  insertHead(intList, 5);
+  // insertHead(intList, 5);
+  // insertHead(intList, 4);
+  // insertHead(intList, 3);
+  // insertHead(intList, 2);
+  // insertHead(intList, 1);
+  insertNodeTail(intList, 6);
 
   printLinkedList(intList);
 
